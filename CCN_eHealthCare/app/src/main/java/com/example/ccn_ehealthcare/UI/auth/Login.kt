4fc2532954f96c3dbdbc3a1cea2ace4798dced7b
Yu Lim Kim <yulime0605@naver.com>
2021-05-04 13:48:18 +0900

    activity

---
+++ CCN_eHealthCare/app/src/main/java/com/example/ccn_ehealthcare/UI/auth/Login.kt
@@ -0,0 +1,11 @@
+package com.example.ccn_ehealthcare.UI.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
