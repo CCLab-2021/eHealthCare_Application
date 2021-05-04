79a844cc88203d4b2e13d09a03d9c61fd3b4f213
Yu Lim Kim <yulime0605@naver.com>
2021-05-04 19:12:30 +0900

    //

--- CCN_eHealthCare/app/src/main/java/com/example/ccn_ehealthcare/UI/auth/Login.kt
+++ CCN_eHealthCare/app/src/main/java/com/example/ccn_ehealthcare/UI/auth/Login.kt
@@ -1,12 +1,17 @@
 package com.example.ccn_ehealthcare.UI.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ccn_ehealthcare.R
+import com.example.ccn_ehealthcare.UI.doctor.DoctorHome
+import com.example.ccn_ehealthcare.UI.patient.PatientHome
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
 }
