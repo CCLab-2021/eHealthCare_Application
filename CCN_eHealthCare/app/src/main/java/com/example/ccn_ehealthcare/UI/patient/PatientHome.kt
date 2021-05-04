fa7e3047a3a6051d7cbb12d2d41727b133a9cfd8
JaesungLeee <viki0114@naver.com>
2021-05-04 14:30:59 +0900

    feat : add UI Componets (activities)

---
+++ CCN_eHealthCare/app/src/main/java/com/example/ccn_ehealthcare/UI/patient/PatientHome.kt
@@ -0,0 +1,12 @@
+package com.example.ccn_ehealthcare.UI.patient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ccn_ehealthcare.R
+
+class PatientHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_home)
+    }
+}
