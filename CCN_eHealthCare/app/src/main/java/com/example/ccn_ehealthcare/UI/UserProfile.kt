b31507bd408962d1247c702b92a0216836f8f2db
JaesungLeee <viki0114@naver.com>
2021-05-09 13:17:53 +0900

    feat : user info 업데이트, password 변경 추가 (다시 로그인 시 에러 확인 필요)

--- CCN_eHealthCare/app/src/main/java/com/example/ccn_ehealthcare/UI/UserProfile.kt
+++ CCN_eHealthCare/app/src/main/java/com/example/ccn_ehealthcare/UI/UserProfile.kt
@@ -2,11 +2,112 @@ package com.example.ccn_ehealthcare.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import com.example.ccn_ehealthcare.R
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_user_profile.*
 
 class UserProfile : AppCompatActivity() {
+
+    companion object {
+        private var USERUID = "USERUID"
+        private var USERNICKNAME = "USERNICKNAME"
+        private var USERFULLNAME = "USERFULLNAME"
+        private var USEREMAIL = "USEREMAIL"
+        private var USERPW = "USERPW"
+        private var USERBIRTH = "USERBIRTH"
+        private var USERTYPE = "USERTYPE"
+    }
+
+    var userUid = ""
+    var userNickName = ""
+    var userFullName = ""
+    var userEmail = ""
+    var userPW = ""
+    var userBirth = ""
+    var userType = ""
+
+    var databaseReference : DatabaseReference? = null
+    var database : FirebaseDatabase? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database?.reference!!.child("User")
+
+        userUid = intent.getStringExtra(USERUID).toString()
+        userNickName = intent.getStringExtra(USERNICKNAME).toString()
+        userEmail = intent.getStringExtra(USEREMAIL).toString()
+        userPW = intent.getStringExtra(USERPW).toString()
+        userType = intent.getStringExtra(USERTYPE).toString()
+        userFullName = intent.getStringExtra(USERFULLNAME).toString()
+        userBirth = intent.getStringExtra(USERBIRTH).toString()
+
+        buttonHandler()
+    }
+
+    private fun buttonHandler() {
+        changePW_btn.setOnClickListener {
+            changePassword()
+        }
+
+        profileSave_btn.setOnClickListener {
+            updateUserInfo()
+        }
+
+        profileBack_btn.setOnClickListener {
+            finish()
+        }
+
+    }
+
+    private fun changePassword() {
+        val previousPW = previousPW_eT.text.toString()
+        val newPW = changePW_eT.text.toString()
+        val confirmNewPW = changeCPW_eT.text.toString()
+
+        if (previousPW == userPW) {
+            if (newPW == confirmNewPW) {
+
+                userPW = newPW
+                updatePW(userPW)
+
+            } else Toast.makeText(applicationContext, "Password is not same", Toast.LENGTH_SHORT).show()
+
+        } else Toast.makeText(applicationContext, "Check Your Password", Toast.LENGTH_SHORT).show()
+
+
+    }
+
+    private fun updateUserInfo() {
+        val updateFullName = userFullName_eT.text.toString()
+        val userBirthY = datePicker.year
+        val userBirthM = datePicker.month
+        val userBirthD = datePicker.dayOfMonth
+
+        userBirth = "$userBirthY-$userBirthM-$userBirthD"
+
+        userFullName = if (!TextUtils.isEmpty(updateFullName)) {
+            updateFullName
+        } else ""
+
+        updateInfo(userBirth, userFullName)
+    }
+
+    private fun updateInfo(userFullName : String, userBirth : String) {
+        val userDBReference = databaseReference?.child(userUid)
+
+        userDBReference?.child("userFullName")!!.setValue(userFullName)
+        userDBReference?.child("userBirth")!!.setValue(userBirth)
+
+    }
+
+    private fun updatePW(userPW: String) {
+        val userDBReference = databaseReference?.child(userUid)
+        userDBReference?.child("userPW")!!.setValue(userPW)
     }
 }
