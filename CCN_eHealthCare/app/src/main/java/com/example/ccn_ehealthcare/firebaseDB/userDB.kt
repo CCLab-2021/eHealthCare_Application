010277295917d3ece9ab21f9fa31f355c10e957a
JaesungLeee <viki0114@naver.com>
2021-05-09 13:18:20 +0900

    style : rename 작업

--- CCN_eHealthCare/app/src/main/java/com/example/ccn_ehealthcare/firebaseDB/userDB.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ccn_ehealthcare.firebaseDB
-
-class userDB(val userID : String, val userNickName : String, val userFullName : String, val userEmail : String, val userPW : String, val userBirth : String, val userType : String) {
-    constructor() : this("", "", "", "", "", "", "")
-}
