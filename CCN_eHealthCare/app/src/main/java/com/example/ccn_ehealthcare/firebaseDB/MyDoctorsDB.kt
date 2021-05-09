7c901431f65603184df53ca680e2214093b8a136
JaesungLeee <viki0114@naver.com>
2021-05-09 20:26:55 +0900

    feat : Create DB (MyDoctors, MyPatients)

---
+++ CCN_eHealthCare/app/src/main/java/com/example/ccn_ehealthcare/firebaseDB/MyDoctorsDB.kt
@@ -0,0 +1,5 @@
+package com.example.ccn_ehealthcare.firebaseDB
+
+class MyDoctorsDB(val specialty : String) {
+    constructor() : this("")
+}
---
+++ CCN_eHealthCare/app/src/main/java/com/example/ccn_ehealthcare/firebaseDB/MyPatientsDB.kt
@@ -0,0 +1,7 @@
+package com.example.ccn_ehealthcare.firebaseDB
+
+class MyPatientsDB (val patientsAge : String, val patientsAddress : String, val report : String) {
+    constructor() : this("", "", "") {
+
+    }
+}
