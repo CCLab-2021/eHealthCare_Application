3e7adc25b4fb397cfe94d140189b0f62794f5f03
JaesungLeee <viki0114@naver.com>
2021-05-11 16:14:17 +0900

    feat : Doctor - MyPatients에서 report save btn 누를 때 event 기능 추가

--- CCN_eHealthCare/app/src/main/java/com/example/ccn_ehealthcare/UI/adapter/myPatientsAdapter.kt
+++ CCN_eHealthCare/app/src/main/java/com/example/ccn_ehealthcare/UI/adapter/myPatientsAdapter.kt
@@ -1,18 +1,33 @@
 package com.example.ccn_ehealthcare.UI.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ccn_ehealthcare.R
+import com.example.ccn_ehealthcare.UI.doctor.MyPatients
 import com.example.ccn_ehealthcare.UI.model.myPatientsModel
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.doctor_reports_layout.view.*
 
 class myPatientsAdapter(val patientsList : List<myPatientsModel>) : RecyclerView.Adapter<myPatientsAdapter.ViewHolder>(){
-    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
+    private lateinit var clickListener : onItemClickListener
+
+    interface onItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListener(listener: onItemClickListener) {
+
+        clickListener = listener
+    }
+
+
+    class ViewHolder(itemView : View, listener: onItemClickListener) : RecyclerView.ViewHolder(itemView){
 
         var patientName = itemView.patientName_tV
         var patientAge = itemView.patientAge_tV
@@ -21,11 +36,17 @@ class myPatientsAdapter(val patientsList : List<myPatientsModel>) : RecyclerView
         var linearLayout : LinearLayout = itemView.linearLayout
         var expandableLayout : RelativeLayout = itemView.expandable_layout
 
+        init {
+         itemView.reportSave_btn.setOnClickListener {
+             listener.onItemClick(adapterPosition)
+         }
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.doctor_reports_layout, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(view, clickListener)
     }
 
     override fun getItemCount(): Int {
--- CCN_eHealthCare/app/src/main/java/com/example/ccn_ehealthcare/UI/model/myPatientsModel.kt
+++ CCN_eHealthCare/app/src/main/java/com/example/ccn_ehealthcare/UI/model/myPatientsModel.kt
@@ -1,3 +1,5 @@
 package com.example.ccn_ehealthcare.UI.model
 
+import android.widget.Button
+
 data class myPatientsModel(val patientsName : String = "",val patientsAddress : String = "", val patientsAge : String = "", val report : String = "", var expandable : Boolean = false)
