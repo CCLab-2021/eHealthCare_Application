a715bd20a516ff0b3def2a754ed0bc7bef0a527f
JaesungLeee <viki0114@naver.com>
2021-05-18 12:33:32 +0900

    fix : DoctorHome.kt - delete log

--- CCN_eHealthCare/app/src/main/java/com/example/ccn_ehealthcare/UI/doctor/DoctorHome.kt
+++ CCN_eHealthCare/app/src/main/java/com/example/ccn_ehealthcare/UI/doctor/DoctorHome.kt
@@ -42,8 +42,6 @@ class DoctorHome : AppCompatActivity() {
         userBirth = intent.getStringExtra(USERBIRTH).toString()
         userType = intent.getStringExtra(USERTYPE).toString()
 
-        Log.e("USERINFO", "uid : ${userUid}, nickname : ${userNickName}, password : ${userPW}, birth : ${userBirth}")
-
         welcome_tV.text = "Hello $userNickName. Have a nice day."
 
         buttonHandler()
@@ -63,7 +61,6 @@ class DoctorHome : AppCompatActivity() {
         }
 
         dPatients_btn.setOnClickListener {
-            Log.e("CHECK", userNickName)
             val intent = Intent(this, MyPatients::class.java).apply {
                 putExtra(USERNICKNAME, userNickName)
             }
